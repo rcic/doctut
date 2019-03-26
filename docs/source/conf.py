94c5c194f598500083d81339e1784fa632bc5ad6
jamesfgv <jamesjfgv@gmail.com>
2019-03-26 14:12:18 -0500

    james

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,6 +137,7 @@ latex_elements = {
 ''',
     'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
     'printindex': r'\footnotesize\raggedright\printindex',
+
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
