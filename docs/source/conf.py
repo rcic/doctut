74ecbf375f5046bcce1299781dcf48220ae8fcab
jamesfgv <jamesjfgv@gmail.com>
2019-03-26 15:39:31 -0500

    james

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,10 +142,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-#latex_documents = [
-#    (master_doc, 'testCol.tex', u'testCol Documentation',
-#     u'James', 'manual', True),
-#]
+latex_documents = [
+    (master_doc, 'testCol.tex', u'testCol Documentation',
+     u'James', 'manual', False),
+]
 
 # -- Options for manual page output ------------------------------------------
 
