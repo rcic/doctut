b6905f68de775a44eb35b2e67c3037f3c0fcbb46
jamesfgv <jamesjfgv@gmail.com>
2019-03-27 15:14:24 -0500

    james

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,8 +131,8 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-    'preamble': r'\input{estilo1.tex}'
-,
+    'preamble': r'''\input{mystyle.tex.txt},
+''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
